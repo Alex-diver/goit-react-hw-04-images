e2c764882a02d9805e34f57337ecff0f2d75833a
Oleksander <123253746+Alex-diver@users.noreply.github.com>
2023-08-27 16:44:52 +0300

    hooks_app

--- src/api.js
+++ src/api.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '38105470-652721ced6ff5551f65b62ae6';
-const perPage = 12;
 
 export async function AllImages(query, page) {
   const response = await axios.get(BASE_URL, {
@@ -11,7 +10,7 @@ export async function AllImages(query, page) {
       q: query,
       image_type: 'photo',
       orientation: 'horizontal',
-      per_page: perPage,
+      per_page: 12,
       page: page,
     },
   });
